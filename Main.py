47dbdb694e1a0c7825d5a392168b0a7bfe65c03e
Prof Josivaldo <prof_josivaldo@gmx.com>
2024-04-07 21:23:14 -0300

    aula de OO Classe Conta e cliente

---
+++ Main.py
@@ -0,0 +1,14 @@
+
+class Main():
+    pass
+    
+from Cliente import Cliente
+from Conta import Conta    
+
+c1=Cliente("João", "1232-4343")#instaciando objeto cliente
+conta=Conta(c1.nome,6656,0)#instaciando objeto conta
+
+print(conta.titular," Numero: ", conta.numero," Seu saldo: ", conta.saldo,)
+
+
+
